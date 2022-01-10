86a286c603bd8141fab7d63897f1f10beb71a3ab
jk <jakubykriz@gmail.com>
2022-01-10 19:50:32 +0100

    add restore i3 workspaces script

--- init_links.sh
+++ init_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Initiate links between files.
 
-for element in .bash_aliases .local/bin/monitor.sh .local/bin/sound.sh .vimrc .xinitrc .Xmodmap .config/i3/config
+for element in .bash_aliases .local/bin/monitor.sh .local/bin/sound.sh .vimrc .xinitrc .Xmodmap .config/i3/config .local/bin/restore_workspaces
 do
   if ! [ -f ~/$element ]; then
     echo "$element  does not exists, creating now."
