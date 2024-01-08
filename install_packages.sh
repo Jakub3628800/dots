166e9c9cfeb1f1b7b98018f3336b1900cc9e8f32
jk <jakubykriz@gmail.com>
2024-01-08 18:04:53 +0100

    remove ansible, replace with install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
-ansible-playbook ansible/install-packages.yaml -i ansible/hosts --ask-become-pass
+sudo apt-get install -y \
+    vim \
+    flameshot \
+    slack \
+    pavucontrol-qt \
+    xclip \
+    python3-virtualenv \
+    python3.10-venv \
+    vlc \
+    curl \
+    nitrogen \
+    postgresql-client-common \
+    docker.io \
+    docker-compose \
+    redshift \
+    golang-go \
+    zsh \
+    direnv \
+    openvpn \
+    tree \
+    jq \
+    gammastep \
+    gh \
+    htop \
+    pass \
+    expect \
+    tig \
+    py3status
+
+echo "Installing pip packages..."
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Script is supposed to install dependencies in the system, not in a virtual environment. Skipping."
+else
+
+    pip install --user \
+        autorandr==1.14.post1 \
+        tldr==3.2.0
+fi
