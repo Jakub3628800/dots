f9d06e8e8f6533697b75ce31c1be918d43449e3e
Jakub Kriz <jakubykriz@gmail.com>
2024-08-15 22:12:01 +0200

    lint(ruff): add ruff formatter to pre-commit

--- i3/.local/bin/action_checker.py
+++ i3/.local/bin/action_checker.py
@@ -27,33 +27,37 @@ def notification_msg(pr_checks: List[Check]) -> str:
         [f"{i['name']}: {i['result']}" for i in pr_checks]
     )
 
+
 def dt_diff(dt1: str, dt2: str) -> str:
-    return str(datetime.strptime(dt1, "%Y-%m-%dT%H:%M:%SZ") - datetime.strptime(dt2, "%Y-%m-%dT%H:%M:%SZ"))
+    return str(
+        datetime.strptime(dt1, "%Y-%m-%dT%H:%M:%SZ")
+        - datetime.strptime(dt2, "%Y-%m-%dT%H:%M:%SZ")
+    )
 
 
 def pr_checker() -> List[Check]:
     """Returns list of PR checks."""
-    output = subprocess.getoutput(["gh pr checks --json=name,state,link,startedAt,completedAt"])
-    for _ in range(0,10):
+    output = subprocess.getoutput(
+        ["gh pr checks --json=name,state,link,startedAt,completedAt"]
+    )
+    for _ in range(0, 10):
         try:
             output = json.loads(output)
             return [
-                    {
-                        "name": c["name"],
-                        "result": c["state"],
-                        "url": c["link"],
-                        "duration": dt_diff(c["completedAt"], c["startedAt"])
-                    }
-                    for c in output
+                {
+                    "name": c["name"],
+                    "result": c["state"],
+                    "url": c["link"],
+                    "duration": dt_diff(c["completedAt"], c["startedAt"]),
+                }
+                for c in output
             ]
-
 
         except json.decoder.JSONDecodeError:
             time.sleep(1)
             continue
 
     raise SystemExit(1, "Cannot parse gh pr checks output")
-
 
 
 if __name__ == "__main__":
