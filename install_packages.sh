bfa4706b5d8423d75b4a7bf9a5076ecdb270312e
Jakub Kriz <jakubykriz@gmail.com>
2024-08-15 18:58:09 +0200

    fix(pckgs): Remove non-relevant packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,56 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt-get install -y \
     zsh \
     eza \
-	batcat \
+	bat \
     stow \
-    neofetch \
-    direnv \
-	neovim \
     vim \
-    flameshot \
-    slack \
-    pavucontrol-qt \
     xclip \
-    python3-virtualenv \
-    vlc \
 	xcompmgr \
 	nitrogen \
-    curl \
-    nitrogen \
-    postgresql-client-common \
-    redshift \
-    golang-go \
+    python3-virtualenv \
     zsh \
-    direnv \
-    openvpn \
-    tree \
-    jq \
-    gammastep \
-    gh \
-    htop \
-    pass \
-    expect \
     xdotool \
     tig \
-    wdisplays \
-    swaylock \
-    py3status \
-    sqlite3 \
+    pass \
+    pass-extension-otp \
     i3 \
     sway \
     i3lock \
-    pass \
-    pass-extension-otp
+    redshift \
+    swaylock \
+    gh
 
-# Docker
-sudo apt-get install -y \
-    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-echo "Installing pip packages..."
 SYSTEM_VIRTUALENV=$HOME/.local/bin/system_environment
+echo "Installing pip packages in $SYSTEM_VIRTUALENV"
 virtualenv $SYSTEM_VIRTUALENV
-$SYSTEM_VIRTUALENV/bin/pip install autorandr==1.14.post1 \
+$SYSTEM_VIRTUALENV/bin/pip install --upgrade autorandr==1.14.post1 \
                         pre-commit==3.6.0 \
                         ruff==0.4.1 \
-                        virtualenvwrapper==6.1.0
+                        virtualenvwrapper==6.1.0 \
+                        pip
