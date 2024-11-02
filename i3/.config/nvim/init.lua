4d2d87345e2e9601ac90550477450561a6853883
Jakub Kriz <jakubykriz@gmail.com>
2024-11-02 20:21:16 +0100

    nvim telescope

--- i3/.config/nvim/init.lua
+++ i3/.config/nvim/init.lua
@@ -88,7 +88,19 @@ require("lazy").setup({
 			end, { silent = true })
 		end,
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 })
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Basic Neovim settings
 vim.opt.number = true -- Show line numbers
