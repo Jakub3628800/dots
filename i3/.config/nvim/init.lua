fa48d414a5dbe6ddedf5a8ac275c04743a6cd8c2
Jakub Kriz <jakubykriz@gmail.com>
2024-11-02 14:41:55 +0100

    add nvim-tree and lua linter

--- i3/.config/nvim/init.lua
+++ i3/.config/nvim/init.lua
@@ -1,98 +1,135 @@
 -- Bootstrap lazy.nvim
+-- Key mappings
+vim.g.mapleader = " " -- Set leader key to space
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up plugins
 require("lazy").setup({
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'echasnovski/mini.nvim', -- if you use the mini.nvim suite
-      -- 'echasnovski/mini.icons', -- if you use standalone mini plugins
-      -- 'nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "markdown_inline" },
-        auto_install = true,
-        highlight = { enable = true },
-      })
-    end,
-  },
-  -- Add Gruvbox theme
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- Load the colorscheme here
-      vim.o.background = "dark" -- or "light" for light mode
-      vim.cmd([[colorscheme gruvbox]])
-    end,
-  },
-  -- Add other plugins here as needed
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim", -- if you use the mini.nvim suite
+			-- 'echasnovski/mini.icons', -- if you use standalone mini plugins
+			-- 'nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "markdown", "markdown_inline" },
+				auto_install = true,
+				highlight = { enable = true },
+			})
+		end,
+	},
+	-- Add Gruvbox theme
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- Load the colorscheme here
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
+	-- Add other plugins here as needed
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({
+				tab = {
+					sync = {
+						open = true, -- Open tree when new tab is opened
+						close = true, -- Close tree when tab is closed
+						ignore = {}, -- List of buffers to ignore
+					},
+				},
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
+
+					api.config.mappings.default_on_attach(bufnr)
+
+					vim.keymap.set("n", "t", api.node.open.tab, { buffer = bufnr })
+				end,
+			})
+			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>a", function()
+				local nvim_tree = require("nvim-tree.api")
+				local current_win = vim.api.nvim_get_current_win()
+				local current_buf = vim.api.nvim_win_get_buf(current_win)
+				local buf_name = vim.api.nvim_buf_get_name(current_buf)
+
+				if buf_name:match("NvimTree_") then
+					vim.cmd("wincmd p") -- Go to previous window
+				else
+					nvim_tree.tree.focus()
+				end
+			end, { silent = true })
+		end,
+	},
 })
 
 -- Basic Neovim settings
-vim.opt.number = true  -- Show line numbers
-vim.opt.expandtab = true  -- Use spaces instead of tabs
-vim.opt.shiftwidth = 2  -- Size of an indent
-vim.opt.softtabstop = 2  -- Number of spaces tabs count for in insert mode
-vim.opt.smartindent = true  -- Insert indents automatically
+vim.opt.number = true -- Show line numbers
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.softtabstop = 2 -- Number of spaces tabs count for in insert mode
+vim.opt.smartindent = true -- Insert indents automatically
 vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
 
--- Key mappings
-vim.g.mapleader = " "  -- Set leader key to space
-
 -- Define the custom command
-vim.api.nvim_create_user_command('Tabn', 'tabnew | Ex', {})
+vim.api.nvim_create_user_command("Tabn", "tabnew | Ex", {})
 
 -- Map Ctrl+Enter to the custom command
-vim.api.nvim_set_keymap('n', '<C-CR>', ':Tabn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-CR>", ":Tabn<CR>", { noremap = true, silent = true })
 
 -- Define a new command Exh
- vim.api.nvim_create_user_command('Hh', function()
-    local current_file = vim.fn.expand('%:p')
-    local current_dir = vim.fn.fnamemodify(current_file, ':h')
-    vim.cmd('edit ' .. vim.fn.fnameescape(current_dir))
+vim.api.nvim_create_user_command("Hh", function()
+	local current_file = vim.fn.expand("%:p")
+	local current_dir = vim.fn.fnamemodify(current_file, ":h")
+	vim.cmd("edit " .. vim.fn.fnameescape(current_dir))
 end, {})
 
+vim.api.nvim_create_user_command("Cc", function()
+	-- Get the current line
+	local line = vim.api.nvim_get_current_line()
 
+	-- Check if the line contains a checkbox (checked or unchecked)
+	local new_line
+	if line:match("%[[ x]%]") then
+		-- Toggle the checkbox state
+		new_line = line:gsub("%[[ x]%]", function(match)
+			return match == "[ ]" and "[x]" or "[ ]"
+		end, 1)
 
-vim.api.nvim_create_user_command('Cc', function()
-    -- Get the current line
-    local line = vim.api.nvim_get_current_line()
+		-- Get the current cursor position
+		local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-    -- Check if the line contains a checkbox (checked or unchecked)
-    local new_line
-    if line:match('%[[ x]%]') then
-        -- Toggle the checkbox state
-        new_line = line:gsub('%[[ x]%]', function(match)
-            return match == '[ ]' and '[x]' or '[ ]'
-        end, 1)
+		-- Replace the current line with the new line
+		vim.api.nvim_set_current_line(new_line)
 
-        -- Get the current cursor position
-        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-
-        -- Replace the current line with the new line
-        vim.api.nvim_set_current_line(new_line)
-
-        -- Set the cursor position to the same column
-        vim.api.nvim_win_set_cursor(0, {row, col})
-    end
+		-- Set the cursor position to the same column
+		vim.api.nvim_win_set_cursor(0, { row, col })
+	end
 end, {})
--- i3/.wezterm.lua
+++ i3/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,13 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.color_scheme = "AdventureTime"
+config.color_scheme = "Gruvbox dark, medium (base16)"
 config.enable_scroll_bar = true
 
 -- and finally, return the configuration to wezterm
 
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 local act = wezterm.action
 config.disable_default_key_bindings = true
@@ -21,75 +21,74 @@ config.disable_default_key_bindings = true
 config.scrollback_lines = 3500
 config.enable_scroll_bar = true
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 config.keys = {
-  { key = 'y', mods = 'CTRL|SHIFT', action = wezterm.action.ActivateCopyMode },
+	{ key = "y", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
 
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
 
-  { key = 'Enter', mods = 'ALT', action = act.SpawnTab 'DefaultDomain' },
-  { key = 'c', mods = 'ALT', action = wezterm.action.CloseCurrentTab{ confirm = true },},
+	{ key = "Enter", mods = "ALT", action = act.SpawnTab("DefaultDomain") },
+	{ key = "c", mods = "ALT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
 
-  -- Navigate tabs
-  { key = 'h', mods = 'ALT', action = act.ActivateTabRelative(-1) },
-  { key = 'LeftArrow', mods = 'ALT', action = act.ActivateTabRelative(-1) },
-  { key = 'l', mods = 'ALT', action = act.ActivateTabRelative(1) },
-  { key = 'RightArrow', mods = 'ALT', action = act.ActivateTabRelative(1) },
+	-- Navigate tabs
+	{ key = "h", mods = "ALT", action = act.ActivateTabRelative(-1) },
+	{ key = "LeftArrow", mods = "ALT", action = act.ActivateTabRelative(-1) },
+	{ key = "l", mods = "ALT", action = act.ActivateTabRelative(1) },
+	{ key = "RightArrow", mods = "ALT", action = act.ActivateTabRelative(1) },
 
-  -- Move tab position
-  { key = 'h', mods = 'ALT|CTRL', action = act.MoveTabRelative(-1) },
-  { key = 'LeftArrow', mods = 'ALT|CTRL', action = act.MoveTabRelative(-1) },
-  { key = 'l', mods = 'ALT|CTRL', action = act.MoveTabRelative(1) },
-  { key = 'RightArrow', mods = 'ALT|CTRL', action = act.MoveTabRelative(1) },
+	-- Move tab position
+	{ key = "h", mods = "ALT|CTRL", action = act.MoveTabRelative(-1) },
+	{ key = "LeftArrow", mods = "ALT|CTRL", action = act.MoveTabRelative(-1) },
+	{ key = "l", mods = "ALT|CTRL", action = act.MoveTabRelative(1) },
+	{ key = "RightArrow", mods = "ALT|CTRL", action = act.MoveTabRelative(1) },
 
-  { key = '=', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
-  { key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
-  {
-    key = 'C',
-    mods = 'CTRL',
-    action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
-  },
-  { key = 'k', mods = 'ALT', action = act.ScrollByPage(-1) },
-  { key = 'j', mods = 'ALT', action = act.ScrollByPage(1) },
+	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+	{
+		key = "C",
+		mods = "CTRL",
+		action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
+	},
+	{ key = "k", mods = "ALT", action = act.ScrollByPage(-1) },
+	{ key = "j", mods = "ALT", action = act.ScrollByPage(1) },
 }
 
 for i = 1, 8 do
-  -- CTRL+ALT + number to move to that position
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'ALT',
-    action = wezterm.action.ActivateTab(i - 1),
-  })
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
 end
 
 config.mouse_bindings = {
-  -- Right click sends "woot" to the terminal
---  {
---    event = { Down = { streak = 1, button = 'Right' } },
---    mods = 'NONE',
---    action = act.SendString 'woot',
---  },
+	-- Right click sends "woot" to the terminal
+	--  {
+	--    event = { Down = { streak = 1, button = 'Right' } },
+	--    mods = 'NONE',
+	--    action = act.SendString 'woot',
+	--  },
 
-  -- Change the default click behavior so that it only selects
-  -- text and doesn't open hyperlinks
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'NONE',
-    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
-  },
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
 
-  -- and make CTRL-Click open hyperlinks
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
-    action = act.OpenLinkAtMouseCursor,
-  },
-  -- NOTE that binding only the 'Up' event can give unexpected behaviors.
-  -- Read more below on the gotcha of binding an 'Up' event only.
+	-- and make CTRL-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+	-- NOTE that binding only the 'Up' event can give unexpected behaviors.
+	-- Read more below on the gotcha of binding an 'Up' event only.
 }
-
 
 return config
